39275e839043a0f28893d8f261b7611b25566038
aditi-joshi-usc <aajoshi@usc.edu>
2025-04-21 16:55:48 -0700

    update path

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,10 +33,11 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-    app.get('*', (req, res) => {
+    app.get('/{*any}', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html'));
     });
 }
+
 
 server.listen(5001, () => {
     connectDB();
