06dcdd10f56d74fe13f0848b92388f26abb39403
aditi-joshi-usc <aajoshi@usc.edu>
2025-04-21 16:11:55 -0700

    project complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import {connectDB} from './lib/db.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import { app, server } from './lib/socket.js';
 
@@ -17,6 +18,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -28,6 +30,13 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html'));
+    });
+}
 
 server.listen(5001, () => {
     connectDB();
